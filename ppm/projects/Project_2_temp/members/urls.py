736f09c615f2ee6d2717e227cb6f9b7f94d63820
RoccoTescaro <rocco.tescaro@stud.unifi.it>
2024-05-30 20:33:17 +0200

    moved to new repo

--- ppm/projects/Project_2_temp/members/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login', views.loginUser, name="login"),
-    path('logout', views.logoutUser, name="logout"),
-    path('signin', views.signinUser, name="signin"),
-]
--- ppm/projects/Project_2_temp/temp_project/user_context.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AnonymousUser
-
-def usernameContext(request):
-  user = request.user
-  if not isinstance(user, AnonymousUser):
-    username = user.username
-  else:
-    username = None
-  return {'username': username}
